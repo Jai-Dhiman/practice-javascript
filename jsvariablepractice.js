fc47c28a889ec5acb5aa5eb4e5a98961af39a09e
JaiDhiman <jaidhiman2000@gmail.com>
2024-09-15 13:06:20 -0700

    init

---
+++ jsvariablepractice.js
@@ -0,0 +1,126 @@
+// 1. Write a program that uses a variable to store a number, then prints that number plus 10.
+
+// let x = 1;
+// console.log(x + 10);
+
+// 2. Write a program that uses a variable to store a word, then prints that word with all capital letters.
+
+// let x = "word";
+// console.log(x.toUpperCase());
+
+// 3. Write a program that uses variables to store two numbers, then prints the numbers added together.
+
+// let x = 3;
+// let y = 2;
+
+// console.log(x + y);
+
+// 4. Write a program that uses a variable to store a word, then prints that word in reverse order.
+
+// let x = "word";
+// console.log(x.split("").reverse().join(""));
+
+// 5. Write a program that uses a variable to store a number, then prints the number times 10.
+
+// 6. Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+
+// let x = "word";
+// let y = "word2";
+
+// console.log((x + y).toUpperCase());
+
+// 7. Write a program that uses a variable to store a word, then prints the number of letters in the word.
+
+// let x = "word";
+// console.log(x.length);
+
+// 8. Write a program that uses a variable to store a number with decimals, then prints the number as an integer.
+
+// let x = 4.53;
+// console.log(parseInt(x));
+
+// 9. Write a program that uses a variable to store two numbers, then prints the two numbers multiplied together.
+
+// let c = 34;
+// let b = 43;
+
+// console.log(c * b);
+
+// 10. Write a program that uses a variable to store a word, then prints the word with all lowercase letters.
+
+// let x = "WORD";
+// console.log(x.toLowerCase());
+
+// 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+
+// let x = "first ";
+// let y = "last";
+
+// console.log(x + y);
+
+// 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
+// let x = "first";
+// let y = "last";
+
+// console.log(`${x} ${y}`);
+
+// 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
+// global.window = {
+//   prompt: (message) => {
+//     console.log(message);
+//     return "marco";
+//   },
+// };
+
+// let x = window.prompt("Input a word: ");
+// if (x == "marco") {
+//   console.log("polo");
+// }
+
+// 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+
+// 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the #{} operator).
+
+// let x = "yellow";
+// let y = "blue";
+// let z = "green";
+
+// console.log(`colors: ${x}, ${y}, ${z}`);
+
+// 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+
+// global.window = {
+//   prompt: (message) => {
+//     console.log(message);
+//     return "marco";
+//   },
+// };
+
+// let x = window.prompt("Input your name: ");
+// if (x != "santa") {
+//   console.log("You're not Santa");
+// }
+
+// 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+
+// 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
+
+// 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
+
+// global.window = {
+//   prompt: (message) => {
+//     console.log(message);
+//     return "Joshua";
+//   },
+// };
+
+// let x = window.prompt("Password?: ");
+// if (x == "Joshua") {
+//   console.log("Shall we play a game?");
+// } else {
+//   console.log("Access denied");
+// }
+
+// 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
