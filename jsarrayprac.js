e4d3edadce8055cbdde344bd4bf059e347828cb3
JaiDhiman <jaidhiman2000@gmail.com>
2024-09-15 15:02:24 -0700

    prime number

---
+++ jsarrayprac.js
@@ -0,0 +1,50 @@
+// 1. Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
+
+// let people = { firstname: "John", lastname: "Smith", email: "johnsmith" };
+
+// console.log(people.firstname);
+// console.log(people.lastname);
+// console.log(people.email);
+
+// 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+
+// let people = [
+//   { firstname: "P1", lastname: "P2" },
+//   { firstname: "P3", lastname: "P4" },
+//   { firstname: "P5", lastname: "P6" },
+// ];
+
+// console.log(`firstname: ${people[0].firstname}, lastname: ${people[0].lastname}`);
+
+// 3. Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
+
+// let obj = { first: 34, second: 43, third: 12 };
+
+// obj.fourth = 56;
+
+// console.log(obj);
+
+// 4. Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
+
+// let obj = { title: "title", author: "author", pagecount: 45, language: "english" };
+
+// for (let key in obj) {
+//   console.log(`${key}: ${obj[key]}`);
+// }
+
+// Write a function that accepts a string and returns whether it’s a palindrome.
+
+// function palindrome(string) {
+//   if (string === string.split("").reverse().join("")) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// }
+// palindrome("racecars");
+
+// Write a function to generate/print/store the first “n” prime numbers.
+
+function primeNumber(n) {}
+
+primeNumber(7);
